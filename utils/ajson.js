d662d06a854267d478ebf7cb8e1be1e798a61235
Maxim Poletaev <zenwalker2@gmail.com>
2015-03-09 03:38:57 +0300

    Binary modifiers #5

--- utils/ajson.js
+++ utils/ajson.js
@@ -4,24 +4,41 @@ module.exports = {
    * Convert pseudo JSON to object.
    * @public
    *
-   * @param {String} string
+   * @param {String} input
    * @return {*}
    */
-  parse: function(string) {
-    var output = string.replace(/([a-z0-9-]+)/ig, '"$1"');
-    
-    var reObject = new RegExp('^{');
-    var reArray = new RegExp('}[ ]*,[ ]*{');
-    
-    if (!reObject.test(output)) {
-      output = '{' + output + '}';
+  parse: function(input) {
+
+    if (~input.indexOf('{') || ~input.indexOf('[')) {
+      var output = input.replace(/([a-z0-9-]+)/ig, '"$1"');
+      var reArray = new RegExp('}[ ]*,[ ]*{');
+      var reObject = new RegExp('^{');
+
+      if (!reObject.test(output)) {
+        output = '{' + output + '}';
+      }
+
+      if (reArray.test(output)) {
+        output = '[' + output + ']'
+      }
+
+      return JSON.parse(output);
     }
-    
-    if (reArray.test(output)) {
-      output = '[' + output + ']'
+    else {
+      output = (~input.indexOf(':')) ? {} : [];
+
+      input.split(',').forEach(function(sect) {
+        var kv = sect.split(':');
+        if (kv.length > 1) {
+          output[kv[0].trim()] = kv[1].trim();
+        } else {
+          output.push(sect.trim());
+        }
+      });
+
+      return output;
     }
 
-    return JSON.parse(output);
   }
 
 };
--- utils/bem.js
+++ utils/bem.js
@@ -17,25 +17,47 @@ module.exports = function(config) {
   }
 
   function buildModClass(baseClass, modKey, modVal) {
-    return baseClass + config.modPrefix + modKey + config.modDlmtr + modVal;
+    var className = baseClass + config.modPrefix + modKey;
+    if (modVal) className += config.modDlmtr + modVal;
+    return className;
   }
 
   return {
     setClasses: function($this, selector) {
+      var that = this;
+
       $this.addClass(this.buildSelector({
         block: selector.block,
         elem: selector.elem
       }));
 
       if (selector.mod !== undefined) {
-        for (modKey in selector.mod) {
-          var modVal = selector.mod[modKey];
-
-          $this.addClass(this.buildSelector({
+        if (typeof selector.mod == 'string') {
+          $this.addClass(that.buildSelector({
             block: selector.block,
             elem: selector.elem,
-            mod: modKey+':'+modVal
+            mod: selector.mod
           }));
+        }
+        else if (Array.isArray(selector.mod)) {
+          selector.mod.forEach(function(mod) {
+            $this.addClass(that.buildSelector({
+              block: selector.block,
+              elem: selector.elem,
+              mod: mod
+            }));
+          });
+        }
+        else {
+          for (modKey in selector.mod) {
+            var modVal = selector.mod[modKey];
+
+            $this.addClass(this.buildSelector({
+              block: selector.block,
+              elem: selector.elem,
+              mod: modKey+':'+modVal
+            }));
+          }
         }
       }
     },
@@ -51,8 +73,12 @@ module.exports = function(config) {
         }
 
         if (selector.mod !== undefined) {
-          var mod = selector.mod.split(':');
-          result = buildModClass(result, mod[0], mod[1]);
+          if (~selector.mod.indexOf(':')) {
+            var mod = selector.mod.split(':');
+            result = buildModClass(result, mod[0], mod[1])
+          } else {
+            result = buildModClass(result, selector.mod);
+          }
         }
       }
 
